d3aeb6c1e1e81bedb556da9b63c4833fc11debf4
Erikson <eriksonsousa@hotmail.com>
2023-06-17 22:18:16 -0300

    Criando o objeto candidato e populando seus parâmetros.

--- src/TesteCadastro.java
+++ src/TesteCadastro.java
@@ -4,6 +4,11 @@ public class TesteCadastro {
 
     public static void main(String[] args) {
 
+        Candidato candidato = new Candidato("Erikson", "12345678998", new Date("17/04/1994"), Candidato.Sexo.MASC,
+                "Silvia Lucia Mariano", "145", "Ap 14", "Campo das Antas", "Poços de Caldas",
+                "MG", "eriksonssilva94@gmail.com",0,991038045, true, "Analista de Sistemas",
+                "Neo BPO", 2100.00, true, 3000.00, 6000.00,
+                "Java Basico, Powershell, Selenium, C++ Basico,Comunicativo");
 
 
     }
